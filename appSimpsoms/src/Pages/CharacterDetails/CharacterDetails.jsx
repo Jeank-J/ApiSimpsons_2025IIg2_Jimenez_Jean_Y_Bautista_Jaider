dd687c201a56c4a424fcc2070a34b3ba3678235f
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-10-09 12:06:06 -0500

    fetch test funcional en personajes y  ubicaciones

--- appSimpsoms/src/Pages/CharacterDetails/CharacterDetails.jsx
+++ appSimpsoms/src/Pages/CharacterDetails/CharacterDetails.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+
+
 
 const CharacterDetails = () => {
+    const [characters, setCharacters] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://thesimpsonsapi.com/api/characters?page=${page}`)
+            .then(res => res.json())
+            .then(data => {
+                setCharacters(data.results);
+                setTotalPages(data.pages);
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error('Error al obtener personajes:', err);
+                setLoading(false);
+            });
+    }, [page]);
+
     return (
         <div>
-            <h1>Character Details</h1>
+            <h1>Personajes de Los Simpsons</h1>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                <div>
+                    <ul>
+                        {characters.map(character => (
+                            <li key={character.id}>
+                                <h2>{character.name}</h2>
+                                <img src={
+                                    `https://cdn.thesimpsonsapi.com/500${character.portrait_path}`
+                                } alt={character.name} width="100" />
+                                <p>{character.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <div>
+                        <button onClick={() => setPage(prev => Math.max(prev - 1, 1))} disabled={page === 1}>
+                            Anterior
+                        </button>
+                        <span>Pagina {page} de {totalPages}</span>
+                        <button onClick={() => setPage(prev => Math.min(prev + 1, totalPages))} disabled={page === totalPages}>
+                            Siguiente
+                        </button>
+                    </div>
+                </div>
+            )}
+
         </div>
     )
 }
--- appSimpsoms/src/Pages/LocationDetails/LocationDetails.jsx
+++ appSimpsoms/src/Pages/LocationDetails/LocationDetails.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+
 
 const LocationDetails = () => {
+    const [locations, setLocations] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://thesimpsonsapi.com/api/locations?page=${page}`)
+            .then(res => res.json())
+            .then(data => {
+                setLocations(data.results);
+                setTotalPages(data.pages);
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error('Error al obtener personajes:', err);
+                setLoading(false);
+            });
+    }, [page]);
     return (
         <div>
-            <h1>Location Details</h1>
+            <h1>Ubicaciones de Los Simpsons</h1>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                <div>
+                    <ul>
+                        {locations.map(location => (
+                            <li key={location.id}>
+                                <h1>{location.id}</h1>
+                                <h2>{location.name}</h2>
+                                <img src={
+                                    `https://cdn.thesimpsonsapi.com/500${location.image_path}`
+                                } alt={location.name} width="100" />
+                                <p>{location.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <div>
+                        <button onClick={() => setPage(prev => Math.max(prev - 1, 1))} disabled={page === 1}>
+                            Anterior
+                        </button>
+                        <span>Pagina {page} de {totalPages}</span>
+                        <button onClick={() => setPage(prev => Math.min(prev + 1, totalPages))} disabled={page === totalPages}>
+                            Siguiente
+                        </button>
+                    </div>
+                </div>
+            )}
+
         </div>
     )
 }
