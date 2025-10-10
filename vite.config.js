c0bd2eb2f48c2e7df50a96e1bde085a155e8d2eb
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-10-09 22:44:27 -0500

    Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ApiSimpsons_2025IIg2_Jimenez_Jean_Y_Bautista_Jaider/',
+  base: "/ApiSimpsons_2025IIg2_Jimenez_Jean_Y_Bautista_Jaider",
 })
