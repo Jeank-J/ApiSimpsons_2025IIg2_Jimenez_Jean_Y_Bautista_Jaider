ef93ce4dcc46f337a638bcb045103143ff31e165
Jeank-J <jeankjimenezortega@gmail.com>
2025-10-13 21:30:00 -0500

    upd

--- src/Components/CardAuthor/CardAuthor.jsx
+++ src/Components/CardAuthor/CardAuthor.jsx
@@ -1,13 +1,14 @@
 import react, { useState } from "react"
 import { Card } from '@mui/material';
-import imgS from '../../../public/queSuenio.jpeg';
+import imgS from '../../assets/queSuenio.jpeg';
 
 import './CardAuthor.css';
 
 export default function CardAuthor({ autor }) {
 
   const [hovered, setHovered] = useState(false);
-  const IMG = !hovered ? autor.img : imgS;
+  const IMG = hovered ? imgS: autor.img;
+
   return (
     <Card
       className="author-card text-center p-4 shadow"
@@ -16,10 +17,11 @@ export default function CardAuthor({ autor }) {
     >
       <div className="author-img-container mx-auto">
         <img src={IMG} alt={autor.name} className="author-img img-fluid" />
+        
       </div>
-      <h5 className="author-name mt-3 mb-2">{autor[0]}</h5>
+      <h5 className="author-name mt-3 mb-2">{autor.name}</h5>
       <a
-        href={autor[1]}
+        href={autor.linkedin}
         target="_blank"
         rel="noopener noreferrer"
         className="linkedin-link"
